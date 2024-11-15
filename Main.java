30bd8c5099b4e260b01897e5dfefbc117284e90f
fardindadashi <51158854+fardindadashi@users.noreply.github.com>
2024-11-15 18:05:23 +0330

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,35 +13,8 @@ public class Main {
             TelegramBotsApi botsApi = new TelegramBotsApi();
            botsApi.registerBot(new TabloBot());
            new Thread(new TabloBot()).start();
-//             botsApi.registerBot(new CryptoBot());
-//             new Thread(new CryptoBot()).start();
-//            botsApi.registerBot(new TwitterBot());
-//            new Thread(new TwitterBot()).start();
-
-//            new TabloBot().shareHoldersAllRecords2();
-//            if(args.length == 4)
-//                new TabloBot().shareHoldersAllRecords( Integer.valueOf(args[0]), Integer.valueOf(args[1]),
-//                        Integer.valueOf(args[2]), Integer.valueOf(args[3])); // Integer.valueOf(args[0])
-//            else
-//                System.out.println("No Args");
-
-//            new TabloBot().shareHoldersAllRecords( 20210321, 20220319, 1,0); // Integer.valueOf(args[0])
-
-//            System.out.println("main finished");
-//            if(args.length == 1) {
-//                for (int i = 0; i < Integer.valueOf(args[0]); i++) {
-//                    new TabloBot().shareHoldersAllRecords2();
-//                    System.out.println("\n---------step" + (i + 1) + " done");
-//                }
-//            }
-//            else
-//                System.out.println("No Args");
-
-//            new TabloBot().addShareholdersRecords("sh/l01/");
-//            new TabloBot().sortShareholdersRecords();
-//            new TabloBot().shareHoldersAllRecords3();
         }
-        catch (IOException | TelegramApiException e) { // TelegramApiException | InterruptedException |
+        catch (IOException | TelegramApiException e) {
             e.printStackTrace();
             new TabloBot().sendStackTrace(e);
         }
